e1cb25ee71c65f4f9289b5d30c4c2cc6d4f00cb0
Ethan <gilbertethan.projects@gmail.com>
2022-11-06 18:07:17 -0500

    bruh

--- webserver.py
+++ webserver.py
@@ -4,23 +4,41 @@
 #how to send GET vs HEAD request
 
 from socket import *
+import re
+import time
+import os
+
+
+path = "HelloWorld.html"
 serverIP = "127.0.0.1"
 serverPort = 10000
 serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 serverSocket.bind((serverIP, serverPort))
 serverSocket.listen(1)
 
 print ("The server is ready to receive")
 
+#header min 6 lines
+connection = serverPort
+time_sent = ""
+server = serverIP
+time_mod = os.path.getmtime(path)
+timestamp_mod = time.ctime(time_mod)
+content_len = ""
+content_type = ""
+
+
+
 #algorithm
 while True:
 
     connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(2048).decode()
+    request = connectionSocket.recv(2048).decode()
+    #print (request)
+    request_type = re.search("([^ ]+)", request)
+    print (request_type.group())
 
-    if head:
-        connectionSocket.send("yep - is there ;p")
-    else:
-        connectionSocket.send("1010100101010101010100101000101111010101010")
 
+    #connectionSocket.send("bruh")
     connectionSocket.close()
