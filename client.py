0f9ebde1e638a98752d3774868432ea19ef14679
jonathan <jonathanko45@gmail.com>
2022-11-07 13:00:36 -0500

    UDP server setup (hardcoded)

--- client.py
+++ client.py
@@ -3,10 +3,16 @@ from socket import *
 
 serverIP = "127.0.0.1"
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = raw_input("Enter Domain Name: ")
 
-clientSocket.sendto(message.encode(), (serverIP, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print (modifiedMessage.decode())
-clientSocket.close()
+while True:
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+    message = raw_input("Enter Domain Name: ")
+    if message == "end":
+        print("Session ended")
+        clientSocket.close()
+        exit(0)
+
+    clientSocket.sendto(message.encode(), (serverIP, serverPort))
+    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+    print (modifiedMessage.decode())
+    clientSocket.close()
--- server.py
+++ server.py
@@ -1,6 +1,51 @@
 
 from socket import *
 
+domain = ""
+domainType = ""
+domainClass = ""
+TTL = 0
+IP_address = 0
+record = 0
+
+
+def domain_data(domain_name):
+    global domainType
+    global domainClass
+    global TTL
+    global IP_address
+    global domain
+
+    domain = domain_name
+    if domain == "google.com":
+        domainType = "IN"
+        domainClass = 'A'
+        TTL = 260
+        IP_address = ("192.165.1.1", "192.165.1.10")
+    elif domain == "youtube.com":
+        domainType = "IN"
+        domainClass = 'A'
+        TTL = 160
+        IP_address = "192.165.1.2"
+    elif domain == "uwaterloo.ca":
+        domainType = "IN"
+        domainClass = 'A'
+        TTL = 160
+        IP_address = "192.165.1.3"
+    elif domain == "wikipedia.org":
+        domainType = "IN"
+        domainClass = 'A'
+        TTL = 160
+        IP_address = "192.165.1.4"
+    elif domain == "amazon.ca":
+        domainType = "IN"
+        domainClass = 'A'
+        TTL = 160
+        IP_address = "192.165.1.5"
+    else:
+        return 1
+    return 0
+
 
 serverIP = "127.0.0.1"
 serverPort = 12000
@@ -11,7 +56,15 @@ print ("The server is ready to receive")
 while True:
     message, clientAddress = serverSocket.recvfrom(2048)
     print(message)
-    modifiedMessage = message.decode().upper()
-    print(modifiedMessage)
-    print(clientAddress)
+    if domain_data(message):
+        modifiedMessage = "Unknown domain name"
+    elif message == "google.com":
+        modifiedMessage = ("{}: type {}, class {}, TTL {}, addr ({}) {}\n{}: type {}, class {}, TTL {}, addr ({}) {}"
+                           .format(domain, domainType, domainClass, TTL, record, IP_address[0], domain, domainType, domainClass, TTL, record, IP_address[1]))
+    else:
+        modifiedMessage = ("{}: type {}, class {}, TTL {}, addr ({}) {}".format(domain, domainType, domainClass, TTL, record, IP_address))
+    # modifiedMessage = message.decode().upper()
+    # print(modifiedMessage)
+    # print(clientAddress)
     serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+
